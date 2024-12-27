f2be23849f320f90f87a6511d22465727dfaf95e
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-28 00:54:00 +0500

    refactor(components): PageInvalidAppID -> InvalidAppID

---
+++ src/components/InvalidAppID/InvalidAppID.tsx
@@ -0,0 +1,9 @@
+import './InvalidAppID.scss';
+
+export function InvalidAppID() {
+  return (
+    <div class="invalid-app-id">
+      Application ID is missing
+    </div>
+  )
+}
--- src/components/PageInvalidAppID/PageInvalidAppID.tsx
+++
@@ -1,9 +0,0 @@
-import './PageInvalidAppID.scss';
-
-export function PageInvalidAppID() {
-  return (
-    <div class="page-invalid-app-id">
-      Application ID is missing
-    </div>
-  )
-}
