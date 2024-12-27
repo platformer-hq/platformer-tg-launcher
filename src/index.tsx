32cf1a3fe3ff34a81518a44b3323d6e679bb588d
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-27 23:39:45 +0500

    fix(index): properly configure initial parameters extraction

--- src/index.tsx
+++ src/index.tsx
@@ -8,18 +8,23 @@ import { init } from '@/init.js';
 import './index.scss';
 import './mockEnv.js';
 
-init(retrieveLaunchParams().startParam === 'debug' || import.meta.env.DEV);
-
 const lp = retrieveLaunchParams();
-const searchParams = new URLSearchParams(window.location.search);
-// const appId = parseInt(searchParams.get('app_id') || '', 10);
-const appId = 1;
-// const baseUrl = searchParams.get('api_base_url') || 'https://platformer.tg/api/gql';
-const baseUrl = 'http://localhost:10000/gql';
+const searchParams = new URLSearchParams(
+  // Telegram API has a bug replacing & with &amp; for some reason. We are replacing it back.
+  window.location.search.replace(/&amp;/g, '&'),
+);
+
+// Compute the base URL for API requests. It can either be a relative or absolute path.
+const baseUrl = new URL(
+  searchParams.get('api_base_url') || 'https://platformer.tg/api/gql',
+  window.location.origin,
+).toString();
+
+init(lp.startParam === 'debug' || import.meta.env.DEV);
 
 render(() => (
   <Root
-    appId={appId}
+    appId={parseInt(searchParams.get('app_id') || '', 10)}
     baseUrl={baseUrl}
     platform={lp.platform}
     // TODO: We should use launch params raw representation. Otherwise, we may lose some
