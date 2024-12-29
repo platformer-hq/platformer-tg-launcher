e28b1648959dd59f234a5f9175a8ebb668f973c4
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-29 21:00:43 +0500

    refactor(module.d): simplify declaration

--- src/module.d.ts
+++ src/module.d.ts
@@ -1,13 +1,10 @@
 declare module '*.png?process' {
-  interface ImageMeta {
+  export default {} as {
+    blurDataURL: string;
+    height: number;
+    mime: 'image/png';
     src: string;
     srcSet: string;
     width: number;
-    height: number;
-    mime: 'image/png';
-    blurDataURL: string;
-  }
-
-  const meta: ImageMeta;
-  export default meta;
+  };
 }
