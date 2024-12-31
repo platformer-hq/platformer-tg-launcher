682ae6421ea42d219ce718ef839823dc474e68c6
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-31 18:51:11 +0500

    feat(components): move all initial logic to Root

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,93 +1,123 @@
-import { ErrorBoundary, Switch, Match } from 'solid-js';
-import type { Platform } from '@telegram-apps/sdk-solid';
+import { ErrorBoundary, Show, For } from 'solid-js';
+import {
+  type Platform,
+  retrieveLaunchParams,
+  serializeLaunchParams,
+} from '@telegram-apps/sdk-solid';
+import { coerce, create, defaulted, instance, object, string, type StructError } from 'superstruct';
 
-import { InvalidAppID } from '@/components/InvalidAppID.js';
-import { InvalidInitData } from '@/components/InvalidInitData.js';
 import { BootstrapApp } from '@/components/BootstrapApp.js';
+import { RootErrorBoundary } from '@/components/RootErrorBoundary.js';
+import { PositiveIntFromStr } from '@/validation/PositiveIntFromStr.js';
+import { maybe } from '@/validation/maybe.js';
+import { splitExecutionTuple } from '@/helpers/splitExecutionTuple.js';
+import { AppError } from '@/components/AppError/AppError.js';
 
 import './Root.scss';
 
-function ErrorBoundaryError(error: unknown) {
+function Inner() {
+  const lp = retrieveLaunchParams();
+  const [args, error] = splitExecutionTuple<{
+    appID: number;
+    apiBaseURL?: Maybe<string>;
+    fallbackURL?: Maybe<string>;
+    initTimeout?: Maybe<number>;
+    loadTimeout?: Maybe<number>;
+  }, [field: string, reason: string][]>(() => {
+    try {
+      const argsObj = create(
+        new URLSearchParams(
+          // Telegram API has a bug replacing & with &amp; for some reason. We are replacing it back.
+          window.location.search.replace(/&amp;/g, '&'),
+        ),
+        coerce(
+          object({
+            app_id: PositiveIntFromStr,
+            api_base_url: defaulted(string(), 'https://platformer.tg/api/gql'),
+            fallback_url: maybe(string()),
+            init_timeout: maybe(PositiveIntFromStr),
+            load_timeout: maybe(PositiveIntFromStr),
+          }),
+          instance(URLSearchParams),
+          searchParams => Object.fromEntries(searchParams.entries()),
+        ),
+      );
+      return [true, {
+        appID: argsObj.app_id,
+        apiBaseURL: argsObj.api_base_url,
+        fallbackURL: argsObj.fallback_url,
+        initTimeout: argsObj.init_timeout,
+        loadTimeout: argsObj.load_timeout,
+      }];
+    } catch (e) {
+      return [false, (e as StructError).failures().map(f => [f.key, f.message])];
+    }
+  });
+
   return (
-    <div>
-      <p>ErrorBoundary handled error:</p>
-      <blockquote>
-        <code>
-          <Switch fallback={JSON.stringify(error)}>
-            <Match when={typeof error === 'string' ? error : false}>
-              {v => v()}
-            </Match>
-            <Match when={error instanceof Error ? error.message : false}>
-              {v => v()}
-            </Match>
-          </Switch>
-        </code>
-      </blockquote>
-    </div>
+    <main
+      classList={{
+        'root': true,
+        // TODO: We should probably not add this class only when the platform is mobile. We
+        //  should do it only if the platform is mobile and the wrapped mini app wants to solve
+        //  the problem, solved by this class.
+        'root--mobile': ([
+          'android',
+          'android_x',
+          'ios',
+        ] satisfies Platform[]).includes(lp.platform),
+      }}
+    >
+      <Show
+        when={args.ok() && args()}
+        fallback={
+          <AppError
+            title="Configuration is invalid"
+            subtitle={
+              <For each={error()}>
+                {(item, idx) => (
+                  <>
+                    {idx() ? ', ' : ''}
+                    <b>{item[0]}</b>
+                    &nbsp;
+                    <i>({item[1]})</i>
+                  </>
+                )}
+              </For>
+            }
+          />
+        }
+      >
+        {data => (
+          <Show
+            when={lp.initDataRaw}
+            fallback={
+              <AppError
+                title="Init data is missing"
+                subtitle="For some reason, init data is missing. It is more likely that the application was launched improperly"
+              />
+            }
+          >
+            {initData => (
+              <BootstrapApp
+                {...data()}
+                // TODO: We should use launch params raw representation. Otherwise, we may lose some
+                //  useful data.
+                launchParams={serializeLaunchParams(lp)}
+                initData={initData()}
+              />
+            )}
+          </Show>
+        )}
+      </Show>
+    </main>
   );
 }
 
-export function Root(props: {
-  /**
-   * The application identifier to display.
-   */
-  appId?: Maybe<number>;
-  /**
-   * API base URL.
-   */
-  apiBaseURL: string;
-  /**
-   * Fallback URL to use in case something went wrong with Platformer.
-   */
-  fallbackUrl?: Maybe<string>;
-  /**
-   * Mini app init data.
-   */
-  initData?: Maybe<string>;
-  /**
-   * Mini app launch parameters.
-   */
-  launchParams: string;
-  /**
-   * Mini Apps platform name.
-   */
-  platform: Platform;
-}) {
+export function Root() {
   return (
-    <ErrorBoundary fallback={ErrorBoundaryError}>
-      <main
-        classList={{
-          'root': true,
-          'root--mobile': ([
-            'android',
-            'android_x',
-            'ios',
-          ] satisfies Platform[]).includes(props.platform),
-        }}
-      >
-        <Switch>
-          <Match when={!props.appId}>
-            <InvalidAppID/>
-          </Match>
-          <Match when={!props.initData}>
-            <InvalidInitData/>
-          </Match>
-          <Match
-            when={props.appId && props.initData
-              ? [props.appId, props.initData] as const
-              : false}
-          >
-            {tuple => (
-              <BootstrapApp
-                appId={tuple()[0]}
-                apiBaseURL={props.apiBaseURL}
-                launchParams={props.launchParams}
-                initData={tuple()[1]}
-              />
-            )}
-          </Match>
-        </Switch>
-      </main>
+    <ErrorBoundary fallback={RootErrorBoundary}>
+      <Inner/>
     </ErrorBoundary>
   );
 }
