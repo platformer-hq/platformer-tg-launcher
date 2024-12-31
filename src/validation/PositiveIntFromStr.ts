87b581378a29a3ef7dc01d8823d227fa83bfea1c
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-31 18:50:10 +0500

    feat(validation): preserve initial value in PositiveIntFromStr

--- src/validation/PositiveIntFromStr.ts
+++ src/validation/PositiveIntFromStr.ts
@@ -3,5 +3,8 @@ import { coerce, min, integer, string } from 'superstruct';
 export const PositiveIntFromStr = coerce(
   min(integer(), 1),
   string(),
-  v => parseInt(v, 10),
+  v => {
+    // Preserving v to display the initial value in the error.
+    return parseInt(v, 10) || v;
+  },
 );
