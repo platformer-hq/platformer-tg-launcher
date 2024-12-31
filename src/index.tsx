514ed483968e8c0281150f25c4b2e3e0928a400b
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-31 18:34:56 +0500

    refactor: move all index logic to Root

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,14 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-import { retrieveLaunchParams, serializeLaunchParams } from '@telegram-apps/sdk-solid';
+import { retrieveLaunchParams } from '@telegram-apps/sdk-solid';
 
-import { Root } from '@/components/Root/Root.js';
 import { init } from '@/init.js';
+import { Root } from '@/components/Root/Root.js';
 
 import './index.scss';
 import './mockEnv.js';
 
-const lp = retrieveLaunchParams();
-const searchParams = new URLSearchParams(
-  // Telegram API has a bug replacing & with &amp; for some reason. We are replacing it back.
-  window.location.search.replace(/&amp;/g, '&'),
-);
+const { startParam } = retrieveLaunchParams();
+init(startParam && startParam.includes('platformer_debug') || import.meta.env.DEV);
 
-// Compute the base URL for API requests. It can either be a relative or absolute path.
-const apiBaseURL = new URL(
-  searchParams.get('api_base_url') || 'https://platformer.tg/api/gql',
-  window.location.origin,
-).toString();
-
-init(lp.startParam === 'debug' || import.meta.env.DEV);
-
-render(() => (
-  <Root
-    appId={parseInt(searchParams.get('app_id') || '', 10)}
-    apiBaseURL={apiBaseURL}
-    platform={lp.platform}
-    fallbackUrl={searchParams.get('fallback_url')}
-    // TODO: We should use launch params raw representation. Otherwise, we may lose some
-    //  useful data.
-    launchParams={serializeLaunchParams(lp)}
-    initData={lp.initDataRaw}
-  />
-), document.getElementById('root')!);
+render(() => <Root/>, document.getElementById('root')!);
