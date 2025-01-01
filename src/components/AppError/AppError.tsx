d94995e161f265de199bda973683d745496a4bae
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-01 21:55:30 +0500

    refactor(components): switch AppError to StatusPage

--- src/components/AppError/AppError.tsx
+++ src/components/AppError/AppError.tsx
@@ -1,23 +1,7 @@
-import { type JSXElement, Show } from 'solid-js';
-
-import { Picture } from '@/components/Picture/Picture.js';
+import { StatusPage, type StatusPageProps } from '@/components/StatusPage/StatusPage.js';
 
 import image from './sad.png?process';
-import './AppError.scss';
 
-export function AppError(props: {
-  title: JSXElement;
-  subtitle?: JSXElement;
-}) {
-  return (
-    <div class="app-error">
-      <Picture {...image} class="app-error__image"/>
-      <h1 class="app-error__title">{props.title}</h1>
-      <Show when={props.subtitle}>
-        <p class="app-error__subtitle">
-          {props.subtitle}
-        </p>
-      </Show>
-    </div>
-  );
+export function AppError(props: Pick<StatusPageProps, 'title' | 'subtitle'>) {
+  return <StatusPage {...props} image={image}/>;
 }
