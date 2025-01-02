85f4ec139b7c5e8f32a8758e2e00569a227c912f
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-02 18:37:45 +0500

    refactor(components): refactor Resource props a bit

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -11,22 +11,26 @@ import {
 import type { ExecutionTuple } from '@/types/execution.js';
 import { splitExecutionTuple } from '@/helpers/splitExecutionTuple.js';
 
-/**
- * Helper to split resource results into separate views.
- */
-export function Resource<Data, Err = unknown, Source = object>(props: FlowProps<
+export type ResourceProps<Data, Err = unknown, Source = object> = FlowProps<
   {
     abortSignal?: AbortSignal,
     error?: (error: Accessor<Err>) => JSXElement,
-    fetcher: (
+    fetcher(
       source: Source,
       options: { abortSignal: AbortSignal },
-    ) => ExecutionTuple<Data, Err> | PromiseLike<ExecutionTuple<Data, Err>>;
+    ): ExecutionTuple<Data, Err> | PromiseLike<ExecutionTuple<Data, Err>>;
     loading?: JSXElement;
     source: Source;
   },
   (data: Accessor<Data>) => JSXElement
->) {
+>;
+
+/**
+ * Helper to split resource results into separate views.
+ */
+export function Resource<Data, Err = unknown, Source = object>(
+  props: ResourceProps<Data, Err, Source>,
+) {
   const controller = new AbortController();
   const abortSignal = createMemo(() => props.abortSignal || controller.signal);
 
