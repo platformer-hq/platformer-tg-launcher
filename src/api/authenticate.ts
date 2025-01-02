a2ebecf0e37b353c388fec10b5733c7c3f756433
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-02 18:49:55 +0500

    refactor(api): refactor methods arguments a bit

--- src/api/authenticate.ts
+++ src/api/authenticate.ts
@@ -4,7 +4,7 @@ import { object } from 'superstruct';
 import { gqlRequest, type GqlRequestResult } from '@/api/gqlRequest.js';
 import { AuthToken } from '@/validation/AuthToken.js';
 
-export interface AuthenticateOptions extends AsyncOptions {
+export interface AuthenticateOptions {
   apiBaseURL: string,
   appID: number,
   initData: string,
@@ -12,18 +12,22 @@ export interface AuthenticateOptions extends AsyncOptions {
 
 /**
  * Authenticates the current user.
+ * @param args - execution arguments
  * @param options - execution options
  */
-export function authenticate(options: AuthenticateOptions): CancelablePromise<GqlRequestResult<{ token: string; expiresAt: Date }>> {
+export function authenticate(
+  args: AuthenticateOptions,
+  options?: AsyncOptions,
+): CancelablePromise<GqlRequestResult<{ token: string; expiresAt: Date }>> {
   return gqlRequest(
-    options.apiBaseURL,
+    args.apiBaseURL,
     'mutation ($appID: Int, $initData: String!) {'
     + ' authenticateTelegram(appID: $appID, initData: $initData) {'
     + '  token'
     + '  expiresAt'
     + ' }'
     + '}',
-    { appID: options.appID, initData: options.initData },
+    { appID: args.appID, initData: args.initData },
     object({ authenticateTelegram: AuthToken }),
     options,
   ).then(v => v[0]
--- src/api/getAppUrl.ts
+++ src/api/getAppUrl.ts
@@ -4,7 +4,7 @@ import { object, string } from 'superstruct';
 import { gqlRequest, type GqlRequestResult } from '@/api/gqlRequest.js';
 import { maybe } from '@/validation/maybe.js';
 
-export interface GetAppURLOptions extends AsyncOptions {
+export interface GetAppURLOptions {
   apiBaseURL: string,
   authToken: string,
   appID: number,
@@ -13,23 +13,27 @@ export interface GetAppURLOptions extends AsyncOptions {
 
 /**
  * Retrieves the application URL.
+ * @param args - execution arguments
  * @param options - execution options
  */
-export function getAppUrl(options: GetAppURLOptions): CancelablePromise<GqlRequestResult<[appFound: boolean, url?: Maybe<string>]>> {
+export function getAppUrl(
+  args: GetAppURLOptions,
+  options?: AsyncOptions
+): CancelablePromise<GqlRequestResult<[appFound: boolean, url?: Maybe<string>]>> {
   return gqlRequest(
-    options.apiBaseURL,
+    args.apiBaseURL,
     'query ($appID: Int!, $launchParams: String!) {'
     + ' app(appID: $appID) {'
     + '  telegramURL(launchParams: $launchParams)'
     + ' }'
     + '}',
-    { appID: options.appID, launchParams: options.launchParams },
+    { appID: args.appID, launchParams: args.launchParams },
     object({
       app: maybe(object({
         telegramURL: maybe(string()),
       })),
     }),
-    { ...options, authToken: options.authToken },
+    { ...options, authToken: args.authToken },
   ).then(v => v[0]
     ? [true, v[1].app ? [true, v[1].app.telegramURL] : [false]]
     : v);
