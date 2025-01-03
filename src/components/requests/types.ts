58c380f88e0012866206a255173770d934c2fdfe
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-03 21:19:39 +0500

    refactor(components): remove requests folder

--- src/components/requests/types.ts
+++
@@ -1,6 +0,0 @@
-import type { ResourceProps } from '@/components/Resource.js';
-import type { GqlRequestError } from '@/api/gqlRequest.js';
-
-export type RequestComponentProps<Data, Props = object> =
-  & Omit<ResourceProps<Data, GqlRequestError>, 'source' | 'fetcher'>
-  & Props;
