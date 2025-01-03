9044e319731c769581c9cf182c3159ef8168fcc6
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-03 22:44:27 +0500

    refactor(async): make createAbortSignal accept non-signal value

--- src/async/createAbortSignal.ts
+++ src/async/createAbortSignal.ts
@@ -1,4 +1,4 @@
-import { type Accessor, createEffect } from 'solid-js';
+import { onCleanup } from 'solid-js';
 
 import { TimeoutError } from '@/async/TimeoutError.js';
 
@@ -6,19 +6,15 @@ import { TimeoutError } from '@/async/TimeoutError.js';
  * Creates a new self-cleaning-up abort signal.
  * @param timeout - timeout duration
  */
-export function createAbortSignal(timeout: Accessor<number>): AbortSignal {
+export function createAbortSignal(timeout: number): AbortSignal {
   const controller = new AbortController();
+  const timeoutID = setTimeout(() => {
+    controller.abort(new TimeoutError(timeout));
+  }, timeout);
 
-  createEffect<number>((timeoutID) => {
-    if (timeoutID) {
-      clearTimeout(timeoutID);
-    }
-
-    const v = timeout();
-    return setTimeout(() => {
-      controller.abort(new TimeoutError(v));
-    }, v);
-  })
+  onCleanup(() => {
+    clearTimeout(timeoutID);
+  });
 
   return controller.signal;
 }
