8878e68a7fddc69a3c71dfea74e4a550fbe77a7c
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-04 14:47:56 +0500

    chore(eslint): disable @typescript-eslint/no-explicit-any

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,35 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:solid/typescript'
+    'plugin:solid/typescript',
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
       files: [
-        '.eslintrc.{js,cjs}'
+        '.eslintrc.{js,cjs}',
       ],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
-  rules: {}
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+  },
 };
