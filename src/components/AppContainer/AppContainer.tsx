ec2ad7770a6b9ccedb7d62163b319ddb1f996425
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-04 20:42:07 +0500

    feat(components): actualize AppContainer

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -5,19 +5,37 @@ import { any, create, string, type } from 'superstruct';
 import './AppContainer.scss';
 
 /**
- * Displays the application and configures
+ * Container displaying and configuring wrapped mini application.
  */
 export function AppContainer(props: {
-  url: string;
+  /**
+   * Timeout in milliseconds determining how much time the wrapped mini application has
+   * to load.
+   */
   loadTimeout: number;
+  /**
+   * Will be called whenever the iframe failed to load.
+   */
+  onError(): void;
+  /**
+   * Will be called whenever the application notified about being ready.
+   */
+  onReady(): void;
+  /**
+   * Will be called whenever the application failed to load due to timeout.
+   */
+  onTimeout(): void;
+  /**
+   * URL of the mini application to open.
+   */
+  url: string;
 }) {
   let iframe!: HTMLIFrameElement;
 
-  // TODO: Use loadTimeout
+  // Give some time the mini application to load.
+  // When the timeout is reached, notify the parent component about it.
+  const timeoutID = setTimeout(props.onTimeout, props.loadTimeout);
 
-  // Proxy all Mini Apps events:
-  // 1. From the Telegram client to the mini app.
-  // 2. From the mini app to the Telegram client.
   onMount(() => {
     const { contentWindow } = iframe;
     if (!contentWindow) {
@@ -25,27 +43,50 @@ export function AppContainer(props: {
       return;
     }
 
+    // Define iframe listeners to proxy all methods' calls to the Telegram client.
+    // Also, do the same to proxy events from the Telegram client to the mini application.
     const onMessage = ({ data, source }: MessageEvent) => {
+      let payload: { eventType: string; eventData?: any } | undefined;
       try {
         payload = create(JSON.parse(data), type({
           eventType: string(),
           eventData: any(),
         }));
+      } catch (e) { /* empty */
+      }
+
+      if (payload) {
         if (source === contentWindow) {
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          // Whenever the mini app notifies about it being ready, we should also notify the parent
+          // component.
+          if (payload.eventType === 'web_app_ready') {
+            clearTimeout(timeoutID);
+            props.onReady();
+          }
+
           (postEvent as any)(payload.eventType, payload.eventData);
         } else {
+          // TODO: Set target origin?
           contentWindow.postMessage(data, '*');
         }
-      } catch (e) { /* empty */
-        console.error(e);
       }
     };
+
     window.addEventListener('message', onMessage);
     onCleanup(() => {
       window.removeEventListener('message', onMessage);
     });
   });
 
-  return <iframe ref={iframe} class="app-container" src={props.url}/>;
+  return (
+    <iframe
+      class="app-container"
+      ref={iframe}
+      src={props.url}
+      onError={() => {
+        clearTimeout(timeoutID);
+        props.onError();
+      }}
+    />
+  );
 }
