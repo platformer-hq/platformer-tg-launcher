4ddbeab99b28f9a6d93da2be85dac0e2fa1e7497
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-31 13:54:38 +0500

    refactor(components): expect fallbackURL as string in BootstrapApp handlers

--- src/components/BootstrapApp/BootstrapApp.tsx
+++ src/components/BootstrapApp/BootstrapApp.tsx
@@ -1,9 +1,9 @@
 import {
-  createEffect, createMemo, createRenderEffect,
+  createEffect,
   createSignal,
   Match,
   mergeProps, onMount,
-  Show, splitProps,
+  splitProps,
   Switch,
 } from 'solid-js';
 
@@ -45,8 +45,8 @@ function BasicBootstrap(props: {
   initData: string;
   launchParams: string;
   loadTimeout: number;
-  onError: (error: AppLoadErrorError, fallback?: boolean) => void;
-  onReady: (fallback?: boolean) => void;
+  onError: (error: AppLoadErrorError, fallbackURL?: string) => void;
+  onReady: (fallbackURL?: string) => void;
 }) {
   const [$error, setError] = createSignal<AppLoadErrorError>();
 
@@ -80,10 +80,10 @@ function BasicBootstrap(props: {
                   {...props}
                   url={$url()}
                   onReady={() => {
-                    props.onReady(true);
+                    props.onReady($url());
                   }}
                   onError={error => {
-                    props.onError(error, true);
+                    props.onError(error, $url());
                   }}
                 />
               )}
@@ -123,134 +123,6 @@ function BasicBootstrap(props: {
   );
 }
 
-// /**
-//  * Performs complete application load lifecycle.
-//  */
-// export function BootstrapApp(props: {
-//   apiBaseURL: string;
-//   appID: number;
-//   fallbackURL?: Maybe<string>;
-//   initData: string;
-//   initTimeout: number;
-//   launchParams: string;
-//   loadTimeout: number;
-//   onReady: () => void;
-// }) {
-//   // Contains an error, occurred during designed application bootstrapping flow.
-//   const [$designedFlowError, setDesignedFlowError] =
-//     createSignal<AppLoadErrorError>();
-//   const [$fatalError, setFatalError] = createSignal<AppLoadErrorError>();
-//   const $abortSignal = createTimeoutSignal(() => props.initTimeout);
-//
-//   // Retrieve Platformer authorization token.
-//   const [$authToken] = createExecutionResource(
-//     () => mergeProps(props, { abortSignal: $abortSignal() }),
-//     async options => {
-//       const token = getAuthTokenFromStorage();
-//       return token ? [true, token] : authenticate(options);
-//     },
-//   );
-//
-//   // Retrieve application data.
-//   const [$app] = createExecutionResource(
-//     () => {
-//       const authToken = $authToken.state === 'ready'
-//         ? $authToken().token
-//         : undefined;
-//       return authToken ? mergeProps(props, { authToken }) : false;
-//     },
-//     getAppUrl,
-//   );
-//
-//   // Set error in case something went wrong performing basic operations.
-//   createEffect(() => {
-//     [$app, $authToken].forEach($s => {
-//       $s.state === 'errored' && setDesignedFlowError($s.error);
-//     });
-//   });
-//
-//   // When a fatal error occurs,
-//   createEffect(() => {
-//     $fatalError() && props.onReady();
-//   });
-//
-//   createEffect(() => {
-//     $designedFlowError() && !props.fallbackURL && setFatalError($designedFlowError());
-//   });
-//
-//   return (
-//     <Switch>
-//       <Match when={$fatalError()}>
-//         {$error => <AppLoadError error={$error()}/>}
-//       </Match>
-//       <Match when={$designedFlowError()}>
-//         {$error => (
-//           <Show when={props.fallbackURL} fallback={<AppLoadError error={$error()}/>}>
-//             {$fallbackURL => {
-//               // Create a correct fallback URL. As long as it may have a hash part, we should
-//               // properly append launch parameters.
-//               const url = new URL($fallbackURL());
-//               let hash: string;
-//               if (url.hash) {
-//                 // We should use launch params and merge them with parameters, defined in
-//                 // the URL hash.
-//                 const qp = new URLSearchParams(props.launchParams);
-//                 new URLSearchParams(url.hash.slice(1)).forEach((v, k) => {
-//                   qp.set(k, v);
-//                 });
-//                 hash = qp.toString();
-//               } else {
-//                 hash = props.launchParams;
-//               }
-//               url.hash = `#${hash}`;
-//
-//               const fallbackURL = url.toString();
-//               console.error('Failed to load application:', $error(), `Using fallback: ${fallbackURL}`);
-//               return (
-//                 <AppContainer
-//                   url={fallbackURL}
-//                   onReady={props.onReady}
-//                   onError={() => {
-//                     setDesignedFlowError(['iframe']);
-//                   }}
-//                   onTimeout={() => {
-//                     setDesignedFlowError(['iframe', true]);
-//                   }}
-//                   loadTimeout={props.loadTimeout}
-//                 />
-//               );
-//             }}
-//           </Show>
-//         )}
-//       </Match>
-//       <Match when={$app()}>
-//         {$tuple => (
-//           <Switch fallback={<AppNoURL/>}>
-//             <Match when={!$tuple()[0]}>
-//               <AppNotFound/>
-//             </Match>
-//             <Match when={$tuple()[1]}>
-//               {$url => (
-//                 <AppContainer
-//                   url={$url()}
-//                   onReady={props.onReady}
-//                   onError={() => {
-//                     setDesignedFlowError(['iframe']);
-//                   }}
-//                   onTimeout={() => {
-//                     setDesignedFlowError(['iframe', true]);
-//                   }}
-//                   loadTimeout={props.loadTimeout}
-//                 />
-//               )}
-//             </Match>
-//           </Switch>
-//         )}
-//       </Match>
-//     </Switch>
-//   );
-// }
-
 /**
  * Performs complete application load lifecycle.
  */
@@ -264,8 +136,7 @@ export function BootstrapApp(props: {
   loadTimeout: number;
   onReady: () => void;
 }) {
-  const [$error, setError] =
-    createSignal<AppLoadErrorError>();
+  const [$error, setError] = createSignal<AppLoadErrorError>();
   const $abortSignal = createTimeoutSignal(() => props.initTimeout);
 
   return (
@@ -277,13 +148,13 @@ export function BootstrapApp(props: {
         <BasicBootstrap
           abortSignal={$abortSignal()}
           {...props}
-          onError={(error, fallback) => {
-            fallback && console.error('Fallback URL failed to load:', props.fallbackURL);
+          onError={(error, fallbackURL) => {
+            fallbackURL && console.error('Fallback URL failed to load:', fallbackURL);
             setError(error);
             props.onReady();
           }}
-          onReady={(fallback) => {
-            fallback && console.warn('Platformer failed to load. Used fallback:', props.fallbackURL);
+          onReady={(fallbackURL) => {
+            fallbackURL && console.warn('Platformer failed to load. Used fallback:', fallbackURL);
             props.onReady();
           }}
         />
