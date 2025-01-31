5eaaf34be179617488b70e1185dacde5b397f1c0
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-31 17:17:47 +0500

    feat(api): set message ion GqlError

--- src/api/GqlError.ts
+++ src/api/GqlError.ts
@@ -6,4 +6,5 @@ export const [GqlError] = errorClassWithData<{
 }, [code: string, message?: Maybe<string>]>(
   'GqlError',
   (code, message) => ({ code, message }),
+  (_code, message) => [message || ''],
 );
