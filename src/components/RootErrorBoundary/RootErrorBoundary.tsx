5240654636af2596ad0fae897a980d519b9359b8
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-31 18:02:31 +0500

    feat(components): re-use AppLoadError in RootErrorBoundary

--- src/components/RootErrorBoundary/RootErrorBoundary.tsx
+++ src/components/RootErrorBoundary/RootErrorBoundary.tsx
@@ -1,24 +1,8 @@
-import { Match, Switch } from 'solid-js';
+import { AppLoadError } from '@/components/AppLoadError/AppLoadError.js';
 
 /**
  * Error boundary used in the application's root.
  */
 export function RootErrorBoundary(error: unknown) {
-  return (
-    <div>
-      <p>ErrorBoundary handled error:</p>
-      <blockquote>
-        <code>
-          <Switch fallback={JSON.stringify(error)}>
-            <Match when={typeof error === 'string' ? error : false}>
-              {v => v()}
-            </Match>
-            <Match when={error instanceof Error ? error.message : false}>
-              {v => v()}
-            </Match>
-          </Switch>
-        </code>
-      </blockquote>
-    </div>
-  );
+  return <AppLoadError error={['unknown', error]}/>;
 }
